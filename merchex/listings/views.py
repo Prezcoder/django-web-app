8a26751dde0e30b21de9d675d37f3a83dfaca352
Prezcoder <fbouchar@student.42quebec.com>
2024-02-15 11:10:58 -0500

    MAJ html

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Band
+from listings.models import Listing
+
 
 def hello(request):
-	return HttpResponse('<h1>Hello, world</h1>')
+	bands = Band.objects.all()
+	title = Listing.objects.all()
+	return render(request, 'listings/hello.html',
+        {'bands': bands})
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Listings</h1>')
+
+def contact(request):
+    return HttpResponse('<h1>Contact us</h1> <p>On veut vous entendre !</p>')
 
 # Create your views here.
